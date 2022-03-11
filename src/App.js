51343d63018047e7dfbc40d48492c6cb5257926e
Alexander Raschid <alexraschid1337@gmail.com>
2022-03-10 21:36:06 -0500

    I cant get the markdown api to work with create-react-app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Markdown from 'marked-react';
+
+import { Container, Row, Col } from 'react-bootstrap';
 
 function App() {
+
+  const updatePreview = () => {
+    document.getElementById("preview").innerHTML = <Markdown value={document.getElementById("editor").value}/>;
+  }
+   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Container fluid>
+        <Row >
+          <Col className="text-center"> 
+            <h1 >Markdown Previewer by Alexander R.</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={6}>
+            <textarea id="editor" onKeyUp={updatePreview()}></textarea>
+          </Col>
+          <Col xs={6}>
+            <div id="preview">{}</div>
+          </Col>
+        </Row>
+      </Container>
   );
 }
 
